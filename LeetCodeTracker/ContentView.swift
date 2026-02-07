cc1e1959c0f1b3ad83ac58660c1f9d9f01b09b41
viral-suchak <suchak.v@northeastern.edu>
2026-02-06 23:42:25 -0500

    Initial Commit

---
+++ LeetCodeTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LeetCodeTracker
+//
+//  Created by Viral Suchak on 2/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LeetCodeTracker/LeetCodeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetCodeTrackerApp.swift
+//  LeetCodeTracker
+//
+//  Created by Viral Suchak on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct LeetCodeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
